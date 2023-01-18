3f96d823ffef709b51c664f490a39a6e5d4b509f
hilloftheking <91853499+hilloftheking@users.noreply.github.com>
2023-01-17 19:56:47 -0500

    Attempt to reduce input latency

--- main.c
+++ main.c
@@ -59,14 +59,38 @@ int main(int argc, char *argv[]) {
 		buf->cap = 50;
 		buffer_append(buf, window_title, sizeof window_title - 1);
 
+		// This is used to attempt to synchronize the renderer with vsync so
+		// that events are handled ASAP
+		Uint64 ms_render_interval = 0;
+		Uint64 last_present_tick = 0;
+		SDL_DisplayMode disp_mode = { 0 };
+
 		int should_quit = 0;
 		SDL_StartTextInput();
 		while (!should_quit) {
 			should_quit = program_process();
 
-			SDL_RenderClear(program.renderer);
-			program_draw_text();
-			SDL_RenderPresent(program.renderer);
+			// Only attempt to render a little before the present should be
+			// due. This prevents blocking the event handling
+			if (SDL_GetTicks64() - last_present_tick > ms_render_interval) {
+				Uint64 debug_prepresent = SDL_GetTicks64();
+				SDL_RenderClear(program.renderer);
+				program_draw_text();
+				SDL_RenderPresent(program.renderer);
+				last_present_tick = SDL_GetTicks64();
+
+				// Change the interval at which presenting is attempted
+				// since the refresh rate may have changed
+				SDL_GetCurrentDisplayMode(
+					SDL_GetWindowDisplayIndex(program.window),
+					&disp_mode);
+				// Try to present 1ms early so vsync is beaten
+				ms_render_interval = (1000 / disp_mode.refresh_rate) - 1;
+				if (ms_render_interval == (Uint64)-1) {
+					// Future proofing for 1000hz+
+					ms_render_interval = 0;
+				}
+			}
 		}
 	} while (0);
 
